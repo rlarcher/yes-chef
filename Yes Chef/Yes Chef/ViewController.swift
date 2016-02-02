79f5415326ad6ce5357c6165e5313526a5895b99
Adam Larsen <adam@conversantlabs.com>
2016-02-02 15:18:48 -0500

    Create Xcode project

---
+++ Yes Chef/Yes Chef/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yes Chef
+//
+//  Created by Adam Larsen on 2016/02/02.
+//  Copyright © 2016 Conversant Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
