67857dd241d659d4d6daa8d4bb5bc389d2c3e046
Adam Larsen <adam@conversantlabs.com>
2016-03-03 14:43:19 -0500

    Layout MenuVC

--- Yes Chef/Yes Chef/MenuViewController.swift
+++ Yes Chef/Yes Chef/MenuViewController.swift
@@ -10,5 +10,27 @@ import Foundation
 
 class MenuViewController: UIViewController
 {
+    @IBAction func favoriteRecipesButtonTapped(sender: AnyObject)
+    {
+        // TODO
+        print("MenuViewController favoriteRecipesButtonTapped")
+    }
     
+    @IBAction func groceryListButtonTapped(sender: AnyObject)
+    {
+        // TODO
+        print("MenuViewController groceryListButtonTapped")
+    }
+    
+    @IBAction func helpButtonTapped(sender: AnyObject)
+    {
+        // TODO
+        print("MenuViewController helpButtonTapped")
+    }
+    
+    @IBAction func settingsButtonTapped(sender: AnyObject)
+    {
+        // TODO
+        print("MenuViewController settingsButtonTapped")
+    }
 }
