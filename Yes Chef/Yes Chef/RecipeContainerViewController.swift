e405454833861054d4ab9eb462f244a13a4b1b4e
Adam Larsen <adam@conversantlabs.com>
2016-03-08 16:44:40 -0500

    Wire up RecipeContainerVC

---
+++ Yes Chef/Yes Chef/RecipeContainerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RecipeContainerViewController.swift
+//  Yes Chef
+//
+//  Created by Adam Larsen on 2016/8/3.
+//  Copyright © 2016 Conversant Labs. All rights reserved.
+//
+
+import Foundation
+
+class RecipeContainerViewController: UIViewController
+{
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var recipeCourseLabel: UILabel!
+    @IBOutlet weak var recipeRatingLabel: UILabel!
+    
+    @IBOutlet weak var ingredientsButton: UIButton!
+    @IBOutlet weak var preparationButton: UIButton!
+    @IBOutlet weak var caloriesButton: UIButton!
+    
+    @IBAction func ingredientsButtonTapped(sender: AnyObject)
+    {
+        // TODO
+    }
+    
+    @IBAction func preparationButtonTapped(sender: AnyObject)
+    {
+        // TODO
+    }
+    
+    @IBAction func caloriesButtonTapped(sender: AnyObject)
+    {
+        // TODO
+    }
+}
